eafb63873ef1cfdc114ae3a261a3d704d322a17d
Kobin Kempe <kobinkempe@gmail.com>
2020-10-17 20:05:54 -0500

    bug screen 1

--- app/AtBayPestControl/screens/BugsTabScreen.tsx
+++ app/AtBayPestControl/screens/BugsTabScreen.tsx
@@ -1,32 +1,78 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import {Button, Pressable, ScrollView, StyleSheet} from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
 export default function BugsTabScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Bugs</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/BugsTabScreen.js" />
-    </View>
+      <View>
+        <View style={styles.header}>
+          <Text style={styles.title}>
+            New Price: $10.99
+          </Text>
+          <Button title="Add to Plan" onPress={()=>{}}/>
+        </View>
+        <ScrollView>
+          <View style={styles.container}>
+            <Pressable style={styles.preventionButton} onPress={()=>{}} android_ripple={{color: 'rgba(0,0,0,.15)'}}/>
+            <Pressable style={styles.fullButton} onPress={()=>{}} android_ripple={{color: 'rgba(0,0,0,.15)'}}/>
+            <Pressable style={styles.fullButton} onPress={()=>{}} android_ripple={{color: 'rgba(0,0,0,.15)'}}/>
+            <Pressable style={styles.fullButton} onPress={()=>{}} android_ripple={{color: 'rgba(0,0,0,.15)'}}/>
+            <Pressable style={styles.fullButton} onPress={()=>{}} android_ripple={{color: 'rgba(0,0,0,.15)'}}/>
+            <Pressable style={styles.fullButton} onPress={()=>{}} android_ripple={{color: 'rgba(0,0,0,.15)'}}/>
+            <Pressable style={styles.fullButton} onPress={()=>{}} android_ripple={{color: 'rgba(0,0,0,.15)'}}/>
+            <Pressable style={styles.fullButton} onPress={()=>{}} android_ripple={{color: 'rgba(0,0,0,.15)'}}/>
+          </View>
+        </ScrollView>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    padding: '2.5%',
+    alignItems: 'flex-start',
+    alignContent: 'flex-start',
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+  },
+  header: {
+    padding: '3%',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    backgroundColor: 'rgb(41,41,41)'
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    margin: 10,
   },
   separator: {
     marginVertical: 30,
     height: 1,
     width: '80%',
   },
+  fullButton: {
+    width: '39%',
+    margin: '5.5%',
+    aspectRatio: 1,
+    backgroundColor: 'pink',
+    borderRadius: 20,
+    padding: 10,
+  },
+  preventionButton: {
+    width: '89%',
+    margin: '5.5%',
+    marginBottom: '9%',
+    aspectRatio: 3,
+    backgroundColor: 'rgb(131,195,140)',
+    borderRadius: 20,
+    padding: 10,
+  },
+  ripple: {
+    color: 'rgba(0,0,0,.15)'
+  }
 });
