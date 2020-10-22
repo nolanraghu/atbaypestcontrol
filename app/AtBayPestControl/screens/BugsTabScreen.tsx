28c5805d8de697994630d4fd36efb586445b281a
Kobin Kempe <kobinkempe@gmail.com>
2020-10-22 02:36:40 -0500

    Android ripple, images

--- app/AtBayPestControl/screens/BugsTabScreen.tsx
+++ app/AtBayPestControl/screens/BugsTabScreen.tsx
@@ -1,17 +1,9 @@
 import * as React from 'react';
-import {Button, Pressable, ScrollView, StyleSheet} from 'react-native';
+import {Button, Image, Pressable, ScrollView, StyleSheet, useColorScheme} from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
-interface BugPressProps {
-  button: object
-  //We'll eventually need to put the other parameter here that tell the button which bug it is
-}
-
 export default function BugsTabScreen() {
-
-
   return (
       <View>
         <View style={styles.header}>
@@ -22,14 +14,14 @@ export default function BugsTabScreen() {
         </View>
         <ScrollView style={{marginBottom: '18%'}}>
           <View style={styles.container}>
-            <BugPressable button={styles.preventionButton}/>
-            <BugPressable button={styles.fullButton}/>
-            <BugPressable button={styles.fullButton}/>
-            <BugPressable button={styles.fullButton}/>
-            <BugPressable button={styles.fullButton}/>
-            <BugPressable button={styles.fullButton}/>
-            <BugPressable button={styles.fullButton}/>
-            <BugPressable button={styles.fullButton}/>
+            <BugPressable button={styles.preventionButton} source={require('../assets/images/icon.png')}/>
+            <BugPressable button={styles.fullButton} source={require('../assets/images/ant.png')}/>
+            <BugPressable button={styles.fullButton} source={require('../assets/images/beetle.png')}/>
+            <BugPressable button={styles.fullButton} source={require('../assets/images/blue_beetle.png')}/>
+            <BugPressable button={styles.fullButton} source={require('../assets/images/honey_bee.png')}/>
+            <BugPressable button={styles.fullButton} source={require('../assets/images/icon.png')}/>
+            <BugPressable button={styles.fullButton} source={require('../assets/images/icon.png')}/>
+            <BugPressable button={styles.fullButton} source={require('../assets/images/icon.png')}/>
           </View>
         </ScrollView>
       </View>
@@ -37,19 +29,24 @@ export default function BugsTabScreen() {
 }
 
 
+
 //Sorry Kobin, but you re-typing the specification of the BugButton was giving me
 //anxiety so I did this.
 function BugPressable(props: BugPressProps){
+  const scheme = useColorScheme();
   return(
       <Pressable style={props.button}
                  onPress={()=>{}}
-                 android_ripple={{color: 'rgba(0,0,0,.15)'}
-                 }/>
+                 //This toggles based on the color theme now
+                 android_ripple= {scheme === "dark"? {color: 'rgba(0,0,0,.15)'} : {color: 'rgba(255,255,255,.15)'}}>
+        <Image source={props.source} style={styles.image}/>
+      </Pressable>
   );
 }
 //This is where all the parameters for the BugPressable go
 interface BugPressProps {
   button: object
+  source: object
   //We'll eventually need to put the other parameter here that tell the button which bug it is
 }
 
@@ -86,7 +83,7 @@ const styles = StyleSheet.create({
     aspectRatio: 1,
     backgroundColor: 'pink',
     borderRadius: 20,
-    padding: 10,
+    alignContent: "center"
   },
   preventionButton: {
     width: '89%',
@@ -95,6 +92,10 @@ const styles = StyleSheet.create({
     aspectRatio: 3,
     backgroundColor: 'rgb(131,195,140)',
     borderRadius: 20,
-    padding: 10,
   },
+  image: {
+    width: '85%',
+    height: '85%',
+    alignSelf: "center"
+  }
 });
