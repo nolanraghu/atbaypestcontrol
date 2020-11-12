30a7e553a522a64b1058980b74b6f3de2adce3eb
Kobin Kempe <kobinkempe@gmail.com>
2020-11-12 09:52:37 -0600

    comment

--- app/AtBayPestControl/assets/Classes/User.ts
+++ app/AtBayPestControl/assets/Classes/User.ts
@@ -1,2 +1,3 @@
-//Each User will have a Plan, and information. The payment information can't be stored on the database, but I think
+// Each User will have a Plan, and information. The payment information can't be stored on the database, but I think
 // I read there's a way to have an internal database as well, and this class would be in charge of reading that as well
+// Also the user information will determine all the state details of the app
