0cd48a7753cbdf9c3e3884543efd732b383e160b
Kobin Kempe <kobinkempe@gmail.com>
2020-11-12 10:17:20 -0600

    small edit2

--- app/AtBayPestControl/screens/BugsTabScreen.tsx
+++ app/AtBayPestControl/screens/BugsTabScreen.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
-import {Button, Image, Pressable, ScrollView, StyleSheet, useColorScheme, Text, View} from 'react-native';
+import {Button, Image, Pressable, ScrollView, useColorScheme, Text, View} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import {useState} from "react";
 import {getStyle, buttonColor, getBackgroundColor} from '../assets/Stylesheets/Styles'
 import {getBugInfo} from "../controller/BugPulling";
 
@@ -16,8 +15,8 @@ const bugsData = [
     {bId: "b8", state: 'off', isPreventionButton: false},
 ]
 
-// @ts-ignore
-export default function BugsTabScreen({route, navigation}) {
+export default function BugsTabScreen() {
+    const navigation = useNavigation();
   const scheme = useColorScheme();
   let styles = getStyle(scheme);
 
