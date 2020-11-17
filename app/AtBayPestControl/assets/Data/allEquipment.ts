a3d792740b4c274690666f3d0f5c8a63675988b5
anujamehta <anuja.mehta@vanderbilt.edu>
2020-11-16 22:56:45 -0500

    added data files

---
+++ app/AtBayPestControl/assets/Data/allEquipment.ts
@@ -0,0 +1,25 @@
+// Equipment Objects Stored Here
+
+export const allEquipment = [
+    {
+        id: "e1",
+        image: require("../assets/images/beetle.png"), //TODO: Add better image
+        name: "Spreader",
+        description: "Spreader description",
+        price: 25.99
+    },
+    {
+        id: "e2",
+        image: require("../assets/images/honey_bee.png"), //TODO: Add better image
+        name: "Sprayer",
+        description: "Sprayer description",
+        price: 20.99
+    },
+    {
+        id: "e3",
+        image: require("../assets/images/beetle.png"), //TODO: Add better image
+        name: "Cobweb Duster",
+        description: "Cobweb Duster description",
+        price: 14.99
+    }
+];
--- app/AtBayPestControl/assets/Data/allInfestations.ts
+++ app/AtBayPestControl/assets/Data/allInfestations.ts
@@ -1,12 +1,12 @@
 // Infestation Objects Stored Here
 
-const PDATA: allInfestations[] = [
+export const allInfestations = [
     {
         id: "b1",
         image: require("../assets/images/beetle.png"), //TODO: Add better image
         name: "Base Plan",
         description: "Base Plan description",
-        // products:
+        products: ["p1", "p2"],
         price: 4.99
     },
     {
@@ -14,7 +14,7 @@ const PDATA: allInfestations[] = [
         image: require("../assets/images/honey_bee.png"), //TODO: Add the cockroach
         name: "Cockroach",
         description: "Cockroach description",
-        // products:
+        products: ["p3", "p4", "p5", "p6"],
         price: 5.99
     },
     {
@@ -22,7 +22,7 @@ const PDATA: allInfestations[] = [
         image: require("../assets/images/beetle.png"), //TODO: Add the spider
         name: "Spiders",
         description: "Spiders description",
-        // products:
+        products: ["p7"],
         price: 6.99
     },
     {
@@ -30,17 +30,7 @@ const PDATA: allInfestations[] = [
         image: require("../assets/images/beetle.png"), //TODO: Add the mosquito
         name: "Mosquitoes",
         description: "Mosquitos description",
-        // products:
+        products: ["p8", "p9"],
         price: 5.99
     }
 ];
-
-interface allInfestations{
-    id: String,
-    image: NodeRequire,
-    name: String,
-    description: String,
-    // products: String[],
-    price: number
-
-}
