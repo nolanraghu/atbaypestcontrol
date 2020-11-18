79159238e9046e3a0ce83c6fe0b1ee7269fe08b2
anujamehta <anuja.mehta@vanderbilt.edu>
2020-11-18 18:15:51 -0500

    erghiufeiuw

--- app/AtBayPestControl/assets/Data/allEquipment.ts
+++ app/AtBayPestControl/assets/Data/allEquipment.ts
@@ -3,21 +3,21 @@
 export const allEquipment = [
     {
         id: 0,
-        image: "../images/spreader.jpg", //TODO: Add better image
+        image: 'require ("../images/spreader.jpg")', //TODO: Add better image
         name: "Spreader",
         description: "Spreader description",
         price: 25.99
     },
     {
         id: 1,
-        image: "../images/sprayer.jpg", //TODO: Add better image
+        image: 'require("../images/sprayer.jpg")', //TODO: Add better image
         name: "Sprayer",
         description: "Sprayer description",
         price: 20.99
     },
     {
         id: 2,
-        image: "../images/cobwebDuster.jpeg", //TODO: Add better image
+        image: 'require("../images/cobwebDuster.jpeg")', //TODO: Add better image
         name: "Cobweb Duster",
         description: "Cobweb Duster description",
         price: 14.99
--- app/AtBayPestControl/assets/Data/allProducts.ts
+++ app/AtBayPestControl/assets/Data/allProducts.ts
@@ -5,7 +5,7 @@ import {allEquipment} from "./allEquipment";
 export const allProducts = [
     {
         id: 0,
-        image: "../images/product1.jpg", //TODO: Add the cockroach
+        image: 'require("../images/product1.jpg")', //TODO: Add the cockroach
         name: "Bifen",
         description: "Bifen goes with Basic. Apply it every 6 months!",
         equipment: [0],
@@ -14,7 +14,7 @@ export const allProducts = [
     },
     {
         id: 1,
-        image: "../images/product2.png", //TODO: Add the cockroach
+        image: 'require("../images/product2.png")', //TODO: Add the cockroach
         name: "Suspend SC",
         description: "Suspend SC goes with Basic. Apply it every 6 months!",
         equipment: [1],
@@ -23,7 +23,7 @@ export const allProducts = [
     },
     {
         id: 2,
-        image: '../images/product3.jpg', //TODO: Add the cockroach
+        image: 'require("../images/product3.jpg")', //TODO: Add the cockroach
         name: "Temprid FX",
         description: "Temprid FX works for cockroaches. Apply it once!",
         equipment: [],
@@ -33,7 +33,7 @@ export const allProducts = [
     },
     {
         id: 3,
-        image: "../images/product4.jpeg", //TODO: Add the cockroach
+        image: 'require("../images/product4.jpeg")', //TODO: Add the cockroach
         name: "Catchmaster 1000i Insect Monitors",
         description: "Catchmaster 1000i Insect Monitors works for Cockroaches Apply it once!",
         equipment: [],
@@ -42,7 +42,7 @@ export const allProducts = [
     },
     {
         id: 4,
-        image: "../images/product5.jpeg", //TODO: Add the cockroach
+        image: 'require("../images/product5.jpeg")', //TODO: Add the cockroach
         name: "Maxforce FC MAGNUM",
         description: "Maxforce FC MAGNUM works for cockroaches. Just apply it once!",
         equipment: [],
@@ -51,7 +51,7 @@ export const allProducts = [
     },
     {
         id: 5,
-        image: "../images/honey_bee.png", //TODO: Add the cockroach
+        image: 'require("../images/honey_bee.png")', //TODO: Add the cockroach
         name: "Gentrol Point Source",
         description: "Gentrol Point Source works for cockroaches. Apply it once!",
         equipment: [],
@@ -60,7 +60,7 @@ export const allProducts = [
     },
     {
         id: 6,
-        image: "../images/ant.png", //TODO: Add the cockroach
+        image: 'require("../images/ant.png")', //TODO: Add the cockroach
         name: "CB-80",
         description: "CB-80 works for spiders. Apply as needed!",
         equipment: [2],
@@ -70,7 +70,7 @@ export const allProducts = [
     },
     {
         id: 7,
-        image: "../images/ant2.jpg", //TODO: Add the cockroach
+        image: 'require("../images/ant2.jpg")', //TODO: Add the cockroach
         name: "Talstar P",
         description: "Talstar P works for Mosquitoes. Apply it every 6 months!",
         equipment: [2],
@@ -79,7 +79,7 @@ export const allProducts = [
     },
     {
         id: 8,
-        image: "../images/ant3.jpg", //TODO: Add the cockroach
+        image: 'require("../images/ant3.jpg")', //TODO: Add the cockroach
         name: "Mosquito Beater",
         description: "Mosquito Beater works for spiders... jk mosquitoes. Apply as needed!",
         equipment: [],
