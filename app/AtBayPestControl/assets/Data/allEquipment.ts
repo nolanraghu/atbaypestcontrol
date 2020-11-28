bce4041635f2193dc94211f0774a56df1d87c64d
Nolan R <kickquackstudio@gmail.com>
2020-11-28 13:15:14 -0500

    Equipments professionalized

--- app/AtBayPestControl/assets/Data/allEquipment.ts
+++ app/AtBayPestControl/assets/Data/allEquipment.ts
@@ -3,20 +3,22 @@
 export const allEquipment = [
     {
         id: 0,
-        name: "Spreader",
-        description: "Spreader description",
-        price: 25.99
+        name: "Scotts® Turf Builder® Edgeguard® Mini Broadcast Spreader",
+        description: "Spread product efficiently and effectively with Scotts® Turf Builder® EdgeGuard® Mini Broadcast Spreader. Equipped with Scotts® exclusive EdgeGuard® Technology, you can trust that the product you spread will be spread accurately. You can even control the panel’s precision rate setting to deliver more accurate coverage on up to 5,000 sq. ft. The Scotts® Turf Builder® EdgeGuard® Mini Spreader comes calibrated and ready to use, so you can get started right away.",
+        price: 36.99
     },
     {
         id: 1,
-        name: "Sprayer",
-        description: "Sprayer description",
-        price: 20.99
+        name: "1 Gal. Lawn and Garden and Home Project Sprayer",
+        description: "Ideal for weed control, pest control and fertilizers. " +
+            "Unique in-tank anti-clog filter, for a clog-free, steady spray. " +
+            "Adjustable cone nozzle allows flexibility in use.",
+        price: 12.39
     },
     {
         id: 2,
-        name: "Cobweb Duster",
-        description: "Cobweb Duster description",
-        price: 14.99
+        name: "Industrial Duster - Cobweb",
+        description: "Clean every dusty corner of your office or warehouse.",
+        price: 9.99
     }
 ];
