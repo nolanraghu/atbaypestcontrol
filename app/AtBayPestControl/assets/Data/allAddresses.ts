425b0776693839ea119edf6c3b62efd6693b3aef
anujamehta <anuja.mehta@vanderbilt.edu>
2020-12-01 20:44:52 -0500

    Number ids

--- app/AtBayPestControl/assets/Data/allAddresses.ts
+++ app/AtBayPestControl/assets/Data/allAddresses.ts
@@ -1,6 +1,6 @@
 import Address from "../Classes/Address";
 
 export const LOC = [
-    new Address('1 Real Place', 'New York City', 'New York', '12345'),
-    new Address('2 Real Place', 'Nashville', 'Tennessee', '678910'),
+    new Address(0, '1 Real Place', 'New York City', 'New York', '12345'),
+    new Address(1, '2 Real Place', 'Nashville', 'Tennessee', '678910'),
 ]
