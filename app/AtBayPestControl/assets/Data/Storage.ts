c61136992a83dacc96b9717f3b8022f8260c5e03
Kobin Kempe <kobinkempe@gmail.com>
2020-12-02 15:32:15 -0600

    potentially excessive error handling

--- app/AtBayPestControl/assets/Data/Storage.ts
+++ app/AtBayPestControl/assets/Data/Storage.ts
@@ -28,21 +28,13 @@ export const loadUser = async () => {
 const userDatabase = db.ref('users')
 const passwordDatabase = db.ref('passwords')
 
-export const updateUserOnline = (onError = ()=>{}, onSuccess = ()=>{}):boolean => {
+export const updateUserOnline = (onError = ()=>{}, onSuccess = ()=>{}, onStolenUsername = onError) => {
     if(User.theUser.getID() === '0'){
-        return addNewUser(onError,onSuccess);
+        addNewUser(onError, onSuccess, onStolenUsername);
     } else {
         userDatabase.child(User.theUser.getID()).update({
             userString: User.theUser.toString()
-        })
-
-        let successful = true;
-        if(successful){
-            onSuccess();
-        } else {
-            onError()
-        }
-        return successful;
+        }).then(onSuccess,onError)
     }
 }
 
@@ -50,14 +42,19 @@ export const getUserFromOnline = (username:string,
                                   password:string,
                                   onError = ()=>{},
                                   onSuccess = ()=>{},
-                                  onInvalid = ()=>{}) => {
+                                  onInvalid = ()=>{},
+                                  onDNE = () => {}) => {
     let readUser = (userID:string) => {
         userDatabase.child(userID).once('value').then(
             snapshot => {
-                let userString = snapshot.val().userString;
-                User.theUser.fromString(userString);
-                User.theUser.setID(userID);
-                onSuccess();
+                if (snapshot.val() === null){
+                    onError()
+                } else {
+                    let userString = snapshot.val().userString;
+                    User.theUser.fromString(userString);
+                    User.theUser.setID(userID);
+                    onSuccess();
+                }
             },
             onError
         )
@@ -65,38 +62,44 @@ export const getUserFromOnline = (username:string,
 
     passwordDatabase.child(username).once('value').then(
         snapshot => {
-            let readPassword = snapshot.val().password;
-            let readID = snapshot.val().userID;
-            if (readPassword === password){
-                readUser(readID);
+            if(snapshot.val() === null){
+                onDNE();
             } else {
-                onInvalid();
+                let readPassword = snapshot.val().password;
+                let readID = snapshot.val().userID;
+                if (readPassword === password){
+                    readUser(readID);
+                } else {
+                    onInvalid();
+                }
             }
         },
         onError
     )
 }
 
-export const addNewUser = (onError = ()=>{}, onSuccess = ()=>{}):boolean => {
-    let successful = false;
-    let userKey = userDatabase.push({
-        userString: User.theUser.toString()
-    }).key;
-    if(userKey != null){
+export const addNewUser = (onError = ()=>{}, onSuccess = ()=>{}, onUsernameExists = onError) => {
+    let userKey:string;
+
+    let setKey = (val:any) => {
+        userKey = val.key;
         User.theUser.setID(userKey);
-        passwordDatabase.child(User.theUser.getUserName()).set({
-            userID: userKey,
-            password: User.theUser.getPassword()
-        })
-        successful = true;
+        passwordDatabase.child(User.theUser.getUserName()).once('value').then(
+            snapshot => {
+                if(snapshot.val() === null){
+                    passwordDatabase.child(User.theUser.getUserName()).set({
+                        userID: userKey,
+                        password: User.theUser.getPassword()
+                    }).then(onSuccess, onError);
+                } else {
+                    onUsernameExists();
+                }
+            }, onError
+        )
+
     }
 
-    console.log(User.theUser.toString())
-
-    if(successful){
-        onSuccess();
-    } else {
-        onError()
-    }
-    return successful;
+    userDatabase.push({
+        userString: User.theUser.toString()
+    }).then(setKey, onError);
 }
